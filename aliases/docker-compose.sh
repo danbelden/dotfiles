a253f5e308a91f53a687b66a883db0968595f073
Dan Belden <dan.belden@paymentsense.com>
2021-09-30 09:03:54 +0100

    [Docker] Bump docker compose alias for rebuild

--- aliases/docker-compose.sh
+++ aliases/docker-compose.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 alias docker-compose-up-abort-on-container-exit="docker-compose up --abort-on-container-exit"
-alias docker-compose-up-rebuild="docker-compose up --build --abort-on-container-exit --force-recreate"
-
+alias docker-compose-up-rebuild="docker-compose up --build --abort-on-container-exit --remove-orphans --always-recreate-deps --renew-anon-volumes --force-recreate"
